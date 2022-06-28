1a4eea77346268655b225c9dc514ba94840ec1f0
Re1d0n <reiduninnes@gmail.com>
2022-06-28 15:26:22 -0700

    added Reaction js

--- models/Thought.js
+++ models/Thought.js
@@ -1,36 +1,38 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
-
-const ReactionSchema = new Schema(
-  {
+const reactionSchema = require("./Reaction")
+// const reactionSchema = new Schema(
+//   {
     
-    replyId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    replyBody: {
-      type: String,
-      required: true
-    },
-    writtenBy: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
-);
+//     reactionId: {
+//       type: Schema.Types.ObjectId,
+//       default: () => new Types.ObjectId()
+//     },
+//     reactionBody: {
+//       type: String,
+//       required: true,
+//       maxlength: 280
+//     },
+//     writtenBy: {
+//       type: String,
+//       required: true,
+//       trim: true
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//       get: createdAtVal => dateFormat(createdAtVal)
+//     }
+//   },
+//   {
+//     toJSON: {
+//       getters: true
+//     }, 
+//     id: false
+//   }
+// );
 
-const ThoughtSchema = new Schema(
+const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
@@ -48,7 +50,7 @@ const ThoughtSchema = new Schema(
       required: true,
     },
     reactions: [
-ReactionSchema
+reactionSchema
     ],
 
 
@@ -62,10 +64,10 @@ ReactionSchema
   }
 );
 
-ThoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
